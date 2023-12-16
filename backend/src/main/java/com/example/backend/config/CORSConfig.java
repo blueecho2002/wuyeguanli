02b1c24077e3a2c617fb9abf29684765a155b24b
yz <2092526081@qq.com>
2023-12-16 13:59:31 +0800

    加了依赖

--- backend/src/main/java/com/example/backend/config/CORSConfig.java
+++ backend/src/main/java/com/example/backend/config/CORSConfig.java
@@ -1,4 +1,4 @@
-package com.example.backend.common.config;
+package com.example.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
